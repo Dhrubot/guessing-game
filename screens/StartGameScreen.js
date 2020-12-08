579afa76e68c78bd5297e282274827c59b84b494
Dhrubot <dhrubo.dh@gmail.com>
2020-12-08 14:57:37 -0500

    added a card effect to the input container using stylesheet

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native'
 
-const StartGameScreen = () => {
+const StartGameScreen = props => {
     return (
         <View style={styles.screenView}> 
-            <Text> Start Your Game!</Text>
+            <Text style={styles.title}> Start Your Game!</Text>
+            <View style={ styles.inputContainer }>
+                <Text>Select a Number </Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title='Reset'/>
+                    <Button title='Confirm'/>
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,6 +22,33 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        elevation: 5, // for android card effect
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.26,
+        shadowRadius: 6,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
+
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
 
